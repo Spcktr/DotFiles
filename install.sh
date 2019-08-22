867183eb9a5dd07335f73af0a391123febfa1127
Spcktr <Spcktr@users.noreply.github.com>
2019-08-22 18:27:39 +1000

    removed old aliases

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sudo apt-get install git -y
 sudo apt-get install tmux -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mv ~/dotfiles/.vimrc ~
-mv ~/dotfiles/.bash_aliases ~
 mv ~/dotfiles/.tmux.conf ~
 sudo mv ~/dotfiles/gruvbox.vim /usr/share/vim/vim80/colors/
 vim -c ':PluginInstall' -c ":q" -c ':q'
