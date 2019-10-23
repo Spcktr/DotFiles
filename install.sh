f33ae5ba33db39da784b9777f1099a6124aacc56
Spcktr <Spcktr@users.noreply.github.com>
2019-10-23 08:33:33 +0200

    updated script to be directory agnostic

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 sudo apt-get install vim -y
 sudo apt-get install git -y
 sudo apt-get install tmux -y
+sudo apt-get install fonts-powerline
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mv ~/dotfiles/.vimrc ~
-mv ~/dotfiles/.tmux.conf ~
+mv .vimrc ~
+mv .tmux.conf ~
+mv .tmux.conf.local ~
 sudo mv ~/dotfiles/gruvbox.vim /usr/share/vim/vim80/colors/
 vim -c ':PluginInstall' -c ":q" -c ':q'
 echo "Moves and installs complete, delete folder."
